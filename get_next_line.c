9b6643e930511c38e1d2fec4325de928dcd4eede
Jewoo Lee <jaewoo0254@gmail.com>
2023-11-03 00:46:53 +0900

    modified

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jewoolee <jewoolee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 19:29:11 by jewoolee          #+#    #+#             */
-/*   Updated: 2023/11/02 21:36:56 by jewoolee         ###   ########.fr       */
+/*   Updated: 2023/11/03 00:46:05 by jewoolee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,68 +24,71 @@ static char	*free_all(char *s1, char *s2)
 	return (NULL);
 }
 
-static char	*eof_line(char *s1, char *s2)
+static char	*eof_line(char **ptr, char *s2)
 {
 	char	*line;
 
-	line = gnl_strdup(s1);
+	if (*ptr == NULL)
+		return (NULL);
+	line = gnl_strdup(*ptr);
 	if (line == NULL)
-		return (free_all(s1, s2));
-	s1 = free_all(s1, s2);
+		return (free_all(*ptr, s2));
+	*ptr = free_all(*ptr, s2);
 	return (line);
 }
 
-static char	*backup_line(char *s, size_t from)
+static char	*backup_line(char **s, size_t from)
 {
 	size_t	i;
 	size_t	last;
 	size_t	size;
 	char	*backup;
 
-	last = gnl_strlen(s) + 1;
+	last = gnl_strlen(*s) + 1;
 	size = last - from;
 	backup = (char *)malloc(sizeof(char) * (size + 1));
 	if (backup == NULL)
 	{
-		free(s);
+		free(*s);
 		return (NULL);
 	}
 	i = 0;
 	while (i < size)
 	{
-		backup[i] = s[from + i];
+		backup[i] = (*s)[from + i];
 		i++;
 	}
 	backup[i] = '\0';
-	free(s);
+	free(*s);
+	*s = NULL;
 	return (backup);
 }
 
-static char	*a_line(char *s)
+static char	*a_line(char **s)
 {
 	char	*line;
 	size_t	i;
 	size_t	size;
 
 	size = 0;
-	while (s[size] != '\n')
+	while ((*s)[size] != '\n')
 		size++;
 	line = (char *)malloc(sizeof(char) * (size + 2));
 	if (line == NULL)
 	{
-		free(s);
-		s = NULL;
+		free(*s);
+		*s = NULL;
 		return (NULL);
 	}
 	i = 0;
 	while (i <= size)
 	{
-		line[i] = s[i];
+		line[i] = (*s)[i];
 		i++;
 	}
 	line[i] = '\0';
-	s = backup_line(s, size + 1);
-	if (s == NULL)
+	*s = backup_line(s, size + 1);
+	if (*s == NULL)
 		return (NULL);
 	return (line);
 }
@@ -107,12 +110,12 @@ char	*get_next_line(int fd)
 		if (rd_size < 0)
 			return (free_all(rd_line, buff));
 		if (rd_size == 0)
-			return (eof_line(rd_line, buff));
+			return (eof_line(&rd_line, buff));
 		buff[rd_size] = '\0';
 		rd_line = gnl_strjoin(rd_line, buff);
 		if (rd_line == NULL)
-			return (free_all(rd_line, buff));
 		free(buff);
 	}
-	return (a_line(rd_line));
+	buff = NULL;
+	return (a_line(&rd_line));
 }
